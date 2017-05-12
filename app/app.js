2037d57787aaafe0eb1e50ca2b1e737e8ac60ff0
J. Pichardo <pichardo.software@gmail.com>
2017-05-12 13:37:23 -0700

    Angular Route Fixed

--- app/app.js
+++ app/app.js
@@ -1,14 +1,43 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+var app = angular.module('goCenter', [
+    'ui.router'
+]);
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+app.config(['$urlRouterProvider', '$stateProvider', '$locationProvider', function ($urlRouterProvider, $stateProvider, $locationProvider) {
+
+    $urlRouterProvider.otherwise('/admin');
+    $locationProvider.html5Mode(true);
+
+    var adminState = {
+        name: 'admin',
+        url: '/admin',
+        templateUrl: 'components/admin/admin.view.html',
+        controller: 'adminCtrl'
+    }, adminOrderManagerState = {
+        name: 'adminOrderManager',
+        url: '/admin/orders',
+        templateUrl: 'components/admin/orderManager/admin.orderManager.view.html',
+        controller: 'adminOrderManagerCtrl',
+        parent: 'admin'
+    }, adminProductManagerState = {
+        name: 'adminProductManager',
+        url: '/admin/products',
+        templateUrl: 'components/admin/productManager/admin.productManager.view.html',
+        controller: 'adminProductManagerCtrl',
+        parent: 'admin'
+    }, adminPromotionManagerState = {
+        name: 'adminPromotionManager',
+        url: '/admin/promotions',
+        templateUrl: 'components/admin/promotionManager/admin.promotionManager.view.html',
+        controller: 'adminPromotionManagerCtrl',
+        parent: 'admin'
+    };
+
+    $stateProvider.state(adminState);
+    $stateProvider.state(adminOrderManagerState);
+    $stateProvider.state(adminProductManagerState);
+    $stateProvider.state(adminPromotionManagerState);
+
 }]);
